0a7344a4e06c9123c164efe83490273392e410cb
nehabharati <nsbharati2008@gmail.com>
2020-05-14 11:03:40 +0530

    caeser cipher

---
+++ leetcode_py/02_AE_CaeserCipher.py
@@ -0,0 +1,11 @@
+def caesarCipherEncryptor(string, key):
+    newLetter = []
+	newKey = key % 26
+	for letter in string:
+		newLetter.append(getKey(letter,newKey))
+	return "".join(newLetter)
+
+def getKey(letter,key):
+	nlc = ord(letter) + key
+	return chr(nlc) if nlc <= 122 else chr(96 + nlc % 122)
+ 
